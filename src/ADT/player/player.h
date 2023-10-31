22d2e80fe40b852fedbfb52fe8683657da871017
hodakova <ahmadhabibiem@gmail.com>
2023-10-31 11:21:32 +0700

    to be update

--- src/ADT/player/player.h
+++ src/ADT/player/player.h
@@ -1,9 +1,26 @@
 #ifndef player
 #define player
 
-typedef struct {
-    
-}
+#include "../mesinkata/mesinkata.h"
+#include "../list/list.h"
 
+
+#define nMax 50
+
+typedef Word Lagu;
+
+typedef struct {
+    Word Nama;
+    Lagu Lagu[nMax];
+    int L;
+} Album;
+
+typedef struct {
+    Word Nama;
+    Album Album[nMax];
+    int M;
+} Penyanyi;
+
+typedef 
 
 #endif
