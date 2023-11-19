9f214177c230dde63c0d2670c27b98b6057f3abb
Filbert Fuvian <ffuvian@gmail.com>
2023-11-19 17:13:08 +0700

    Update mesinkarakterv2.c

--- src/ADT/mesinkar/mesinkarakterv2.c
+++ src/ADT/mesinkar/mesinkarakterv2.c
@@ -46,3 +46,17 @@ void ADV()
               fclose(pita);
               printf("menutup\n");}
 }
+
+
+boolean isFileExist(char* dirfile){
+    FILE *file;
+    file = fopen(dirfile, "r");
+    if (file == NULL) {
+       fclose(file);
+       return false;
+    } 
+    else {
+       fclose(file);
+       return true;
+       }
+}
