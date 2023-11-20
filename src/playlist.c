033cc4ae39d83c60c791f740ede9569545db9196
louisferdyo <louisferdyo1028@gmail.com>
2023-11-20 23:34:27 +0700

    Update playlist

--- src/playlist.c
+++ src/playlist.c
@@ -1,14 +1,14 @@
 #include "console.h"
 #include <stdio.h>
 #include <stdlib.h>
-void playlist_create(ArrayDin *playlist) {
+
+void playlist_create(ArrayDin *Playlist) {
    printf("\nMasukkan nama playlist yang ingin dibuat : ");
     STARTWORD();
     currentWordTillSC();
     CreateEmpty(&(Playlist)->DaftarLagu[(Playlist)->Neff]);
-    (Playlist)->NamaPlaylist[(Playlist)->Neff] = playlist_name;
     (Playlist)->Neff += 1;
-    printf("\nOutput: Playlist %s berhasil dibuat!", playlist_name.TabWord);
+    printf("\nOutput: Playlist %s berhasil dibuat!", (Playlist)->NamaPlaylist[(Playlist)->Neff].TabWord);
     printf("\nSilahkan masukkan lagu-lagu artis terkini kesayangan Anda!\n");
 }
 
